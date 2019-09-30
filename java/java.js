8133c6cfd05e85e880db2743580cccca7714b295
sohad <qtaitat3@gmail.com>
2019-10-01 00:00:54 +0300

    use css and js

--- java/java.js
+++ java/java.js
@@ -29,22 +29,22 @@ var userName = function(){
 var type = function (){
         // input
         var favflo = prompt(" Hello :) What occasion you have Wedding or gift flower?  ");
-        var floscn;
+        var floscn='';
       
         // processing:
         if (favflo ==='wedding' ) {
-            floscn = '<img src="https://images.immediate.co.uk/production/volatile/sites/13/2019/01/choose-wedding-flowers-bouquet-thumbnail-75c4c70.jpg?quality=90&resize=620,413">'
-            floscn = '<img src="https://media-api.xogrp.com/images/99337252-d2be-44d7-8cf0-9af4d2b95140~rs_768.h">'
-            floscn = '<img src="https://www.bloominggreenflowers.co.uk/wp-content/uploads/2016/11/wedding_boquet_James-Rouse.jpg">'        
-            floscn = '<img src="https://www.hellomagazine.com/imagenes/brides/2019031270768/how-to-choose-wedding-flowers-each-season/0-345-907/wedding-bouquet-t.jpg">'
+            floscn = floscn.concat('<img src="https://images.immediate.co.uk/production/volatile/sites/13/2019/01/choose-wedding-flowers-bouquet-thumbnail-75c4c70.jpg?quality=90&resize=620,413">');
+            floscn = floscn.concat('<img src="https://media-api.xogrp.com/images/99337252-d2be-44d7-8cf0-9af4d2b95140~rs_768.h">');
+            floscn = floscn.concat('<img src="https://www.bloominggreenflowers.co.uk/wp-content/uploads/2016/11/wedding_boquet_James-Rouse.jpg">');
+            floscn = floscn.concat('<img src="https://www.hellomagazine.com/imagenes/brides/2019031270768/how-to-choose-wedding-flowers-each-season/0-345-907/wedding-bouquet-t.jpg">');
         
           } else if (favflo === 'gift' ) {
            
            
-            movscn = '<img src="https://res.cloudinary.com/bloomnation/c_pad,d_vendor:global:catalog:product:image.png,f_auto,fl_preserve_transparency,q_auto/v1557647751/vendor/6725/catalog/product/2/0/20190128073226_file_5c4f58caa2b73.jpg">'
-            movscn = '<img src="https://www.lesfleursflorist.co.uk/pub/media/catalog/product/cache/2a8d341df1aabef95fa418beb77089e8/s/m/smooth_blues_vase_arrangement.jpg">'
-            movscn = '<img src="http://s7d2.scene7.com/is/image/Teleflora/T43-1C_ENV?&wid=400&fmt=jpeg&qlt=80,0&op_sharpen=0&resMode=bilin&op_usm=3.0,0.5,1.0,0&iccEmbed=0&layer=1&opac=0&layer=2&opac=55&layer=5&opac=0&layer=3&opac=0">'
-            movscn = '<img src="http://www.flowersuganda.com/558-large_default/lovers-flower-gift-special.jpg">'
+            floscn =floscn.concat('<img src="https://res.cloudinary.com/bloomnation/c_pad,d_vendor:global:catalog:product:image.png,f_auto,fl_preserve_transparency,q_auto/v1557647751/vendor/6725/catalog/product/2/0/20190128073226_file_5c4f58caa2b73.jpg">');
+            floscn = floscn.concat( '<img src="https://www.lesfleursflorist.co.uk/pub/media/catalog/product/cache/2a8d341df1aabef95fa418beb77089e8/s/m/smooth_blues_vase_arrangement.jpg">');
+            floscn = floscn.concat('<img src="http://s7d2.scene7.com/is/image/Teleflora/T43-1C_ENV?&wid=400&fmt=jpeg&qlt=80,0&op_sharpen=0&resMode=bilin&op_usm=3.0,0.5,1.0,0&iccEmbed=0&layer=1&opac=0&layer=2&opac=55&layer=5&opac=0&layer=3&opac=0">');
+            floscn = floscn.concat('<img src="http://www.flowersuganda.com/558-large_default/lovers-flower-gift-special.jpg">');
         } else {
             floscn = 'Unknown! ';
         }
@@ -61,18 +61,20 @@ var type = function (){
 
   var showOrder = function() {
     // input
-    var order = prompt('What movie you would like to buy? "wedding" or "gift"?');
+    var order = prompt('What occasion you would like to buy? "wedding" or "gift"?');
     var picture = ''; 
-    var total = NaN;
+    var total ;
   
     // processing
     while (order !== 'wedding' && order !== 'gift') {
       order = prompt('Please enter "wedding" or "gift"...');
     }
-  
-    while(isNaN(total)) {
-      total = prompt('How many Flower would you like?');
-    }
+    
+  do{
+	    total = prompt('How many Flower would you like?');
+  } while(isNaN(total)) 
+    
+    
     
     for(var models = 0; models < total; models = models + 1) {
       if (order === 'wedding') {
